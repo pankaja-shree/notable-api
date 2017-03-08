069a6b453b64941b445eb37d684d44658ea6d380
Pankajashree <pankajashree@gmail.com>
2017-03-08 19:41:43 +0530

    Added CRUD routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,10 +1,26 @@
+//MongoDB requires not just an ID as an ID object 
+//ObjectID, not as a string 
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
-  app.post('/notes/:id', (req, res) => {
-    //create note from post
+
+  //to read a node of particular id - findOne function
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;//grab id from URL parameters
+     const details = { '_id' : new ObjectID(id) };
+       db.collection('notes').findOne(details, (err, item) => {
+         if(err){
+           res.send({'error':'An error has occured'});
+         }
+         else{
+           res.send(item);
+         }
+       });
+  }); 
+
+  //to create a note - insert function
+  app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
-    //creat db collection named notes 
     db.collection('notes').insert(note, (err, result) => {
       if (err) { 
         res.send({ 'error': 'An error has occurred' }); 
@@ -13,4 +29,39 @@ module.exports = function(app, db) {
       }
     });
   });
+
+//to delete a node of particular id - remove function
+//similar to GET but use delete and remove 
+//instead of GET and findOne 
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;//grab id from URL parameters
+     const details = { '_id' : new ObjectID(id) };
+       db.collection('notes').remove(details, (err, item) => {
+         if(err){
+           res.send({'error':'An error has occured'});
+         }
+         else{
+           res.send('Note '+ id +' deleted');
+         }
+       });
+  }); 
+
+//update operation - PUT and update
+//Imperfection - If we fail to give a title or body, the field will be nullified
+//pending work - 
+//add some conditional logic to update the fields only if they’re present in the request
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
+  });
 };
+
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
+  
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });               
